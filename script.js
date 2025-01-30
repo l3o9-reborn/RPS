86c0c3d2d1fde736bd626bb4dc7d33cc6259d320
l3o9 <getleon09@gmail.com>
2025-01-30 14:41:10 +0600

    added user interface and manipulated DOM using js

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-
-let rpsArray=["rock", "paper", "scissors"];
+const buttons=document.querySelectorAll('#rps_list button');
+const resultDiv=document.getElementById('result');
+const yourScore=document.getElementById('userScore');
+const compScore=document.getElementById('computerScore');
+const quickScore=document.getElementById('quickScore');
+const rpsArray=["rock", "paper", "scissors"];
+console.log(buttons)
 
 let humanScore=0;
 let computerScore=0;
+const MAX_SCORE=5;
+
 
 function getComputerChoice(){
-    let rpsRandGen=Math.floor(Math.random()*3);
-    return rpsArray[rpsRandGen];
+
+    return rpsArray[Math.floor(Math.random()*rpsArray.length)];
 }
 
-function getHumanChoice(){
-    let userChoice;
-    do{
-        userChoice=window.prompt("Enter your choice: rock or paper or scissors").toLowerCase();
-    } while (!rpsArray.includes(userChoice));
-    return userChoice;
-}
+// function getHumanChoice(){
+//     let userChoice;
+//     do{
+//         userChoice=window.prompt("Enter your choice: rock or paper or scissors").toLowerCase();
+//     } while (!rpsArray.includes(userChoice));
+//     return userChoice;
+// }
 
 
 function playRound(userChoice, computerChoice){
@@ -25,29 +32,69 @@ function playRound(userChoice, computerChoice){
 
     if (userChoice === computerChoice) {
         console.log("It's a tie!");
+        quickScore.textContent=`Your Choice : ${userChoice}
+                                Computer Choice : ${computerChoice}
+                                result Draw`
     } else if (
         (userChoice === "rock" && computerChoice === "scissors") ||
         (userChoice === "paper" && computerChoice === "rock") ||
         (userChoice === "scissors" && computerChoice === "paper")
     ) {
         humanScore++;
-        console.log(`you win this time and your current score is ${humanScore}`);
+       
+        quickScore.textContent=`Your Choice : ${userChoice}
+                                Computer Choice : ${computerChoice}
+                                result You Win`
 
     } else {
         computerScore++;
-        console.log(`You lose! this time and your opponent score is ${computerScore}`);
+        
+        quickScore.textContent=`Your Choice : ${userChoice}
+                                Computer Choice : ${computerChoice}
+                                result You Lose`
     }
+    resultUpdate();
 }
 
-function playGame(){
-    for (let i=0; i<5; ++i)
+function resultUpdate(){
+    yourScore.textContent= `Your score is : ${humanScore}`;
+    compScore.textContent = `Computer score is : ${computerScore}.`;
+    if(humanScore === MAX_SCORE || computerScore === MAX_SCORE)
     {
-        playRound(getHumanChoice(), getComputerChoice());
+        if(humanScore> computerScore)
+        {
+            resultDiv.textContent=`your score is ${humanScore} 
+                        and computer score is ${computerScore}
+                        Congratulations! You Win!`
+        }
+        else {
+            resultDiv.textContent=`your score is ${humanScore} 
+                        and computer score is ${computerScore}
+                        Oops! You Lost! Try Again`
+        }
+         humanScore=0;
+         computerScore=0;
+
     }
-    console.log(`your score is ${humanScore} and computer score is ${computerScore}`);
-    if(humanScore> computerScore) console.log("YOU WIN!");
-    else if(humanScore< computerScore) console.log("YOU LOSE!");
-    else console.log("YOU TIED@#")
+    else resultDiv.textContent='';
 }
 
-playGame();
+// function result(){
+//     console.log(`your score is ${humanScore} and computer score is ${computerScore}`);
+//     if(humanScore> computerScore) console.log("YOU WIN!");
+//     else console.log("YOU LOSE!");
+
+// }
+
+
+buttons.forEach(button=>{
+    button.addEventListener('click', ()=>{
+        const userChoice=button.textContent.toLowerCase();
+        const computerChoice=getComputerChoice();
+        // playRound(item.innerText, getComputerChoice());
+        console.log(userChoice);
+        playRound(userChoice, computerChoice);
+    });
+});
+
+
